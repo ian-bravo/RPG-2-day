b6098bfcf1b3d5e4a3e29aacb10b5316c39fe69d
Jonathan Cheng <jcheng461@gmail.com>
2023-08-31 14:34:55 -0700

    pass 1st test for calcDmgToReceive function
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- src/js/battle-mechanics.js
+++ src/js/battle-mechanics.js
@@ -1,1 +1,8 @@
-export default function calcDmgToReceive() {}
+export default function calcDmgToReceive(targetOfAtk, incomingDmg) {
+  let damageToHp = incomingDmg - targetOfAtk.defense;
+
+  if (damageToHp <= 0) {
+    damageToHp = 0;
+  }
+  return damageToHp;
+}
