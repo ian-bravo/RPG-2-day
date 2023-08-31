abba44548be155b39ee9fcc3b1dd39d9ed98df42
Jonathan Cheng <jcheng461@gmail.com>
2023-08-31 14:36:50 -0700

    set up and pass 2nd test for calcDmgToReceive
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- __tests__/battle-mechanics.test.js
+++ __tests__/battle-mechanics.test.js
@@ -8,4 +8,11 @@ describe("calcDmgToReceive", () => {
     let damageTaken = calcDmgToReceive(monster, damageDealt);
     expect(damageTaken).toEqual(5);
   });
+
+  test("should take two parameters, the target of attack - class monster or character, and incomingDamage, then takes into consideration this.defense and returns damage taken", () => {
+    const monster = new Monster();
+    let damageDealt = 4;
+    let damageTaken = calcDmgToReceive(monster, damageDealt);
+    expect(damageTaken).toEqual(0);
+  });
 });
