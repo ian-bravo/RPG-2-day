aa8ce5ffd1fde9b3793663ff6029cd2f412aa5c1
Jonathan Cheng <jcheng461@gmail.com>
2023-08-31 16:08:26 -0700

    set up test 1 for testBattle. good fail.
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- __tests__/battle-mechanics.test.js
+++ __tests__/battle-mechanics.test.js
@@ -17,3 +17,30 @@ describe("calcDmgToReceive", () => {
     expect(damageTaken).toEqual(0);
   });
 });
+
+describe("testBattle", () => {
+  test("should instantiate a character and a monster. Monster should take damage, reducing hp.", () => {
+    const character = new Character(10, 6, 4);
+    const monster = new Monster();
+
+    character.pAttackPowerDeterminer();
+    let inflictedDmg = character.pAttackPower;
+
+    let dmgTaken = calcDmgToReceive(monster, inflictedDmg);
+    monster.updateHp(dmgTaken);
+    console.log(`Monster hp is: ${monster.hp}`);
+    expect(monster.hp).toEqual(95);
+
+    let dmgTaken2 = calcDmgToReceive(monster, inflictedDmg);
+    monster.updateHp(dmgTaken2);
+    console.log(`Monster hp is: ${monster.hp}`);
+    expect(monster.hp).toEqual(90);
+
+    let dmgTaken3 = calcDmgToReceive(monster, 1000);
+    monster.updateHp(dmgTaken3);
+    monster.updateAliveStatus();
+    console.log(`Monster hp is: ${monster.hp}`);
+    expect(monster.hp).toEqual(0);
+    console.log(`Monster alive status: ${monster.alive}`);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import css directory here
 // import constructors here
+import Character from "./js/character";
+import Monster from "./js/monster";
+import calcDmgToReceive from "./js/battle-mechanics";
+
+// Test battle
+const character = new Character(10, 6, 4);
+const monster = new Monster();
+
+character.pAttackPowerDeterminer();
+let inflictedDmg = character.pAttackPower;
+
+let dmgTaken = calcDmgToReceive(monster, inflictedDmg);
+monster.updateHp(dmgTaken);
