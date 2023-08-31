36ad3457d3e41150cbcb96fa7bd13572e9e9055c
Jonathan Cheng <jcheng461@gmail.com>
2023-08-31 13:44:44 -0700

    set up test 1 for calcDmgToReceive. good fail.
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>

---
+++ __tests__/battle-mechanics.test.js
@@ -0,0 +1,11 @@
+import calcDmgToReceive from "../src/js/battle-mechanics";
+import Monster from "../src/js/monster";
+
+describe("calcDmgToReceive", () => {
+  test("should take two parameters, the target of attack - class monster or character, and incomingDamage, then takes into consideration this.defense and returns damage taken", () => {
+    const monster = new Monster();
+    let damageDealt = 10;
+    let damageTaken = calcDmgToReceive(monster, damageDealt);
+    expect(damageTaken).toEqual(5);
+  });
+});
